25801c44636bc5f1fe180afc9992c350ee92e2cb
Daniel Haycraft <dhcopy1@gmail.com>
2023-04-25 11:59:25 -0700

    idk whats wrong

--- runner.js
+++ runner.js
@@ -1,7 +1,19 @@
 const { spawn } = require('child_process');
 
-const process = spawn('python3', ['/Users/danielhaycraft/Desktop/my_program/alert.py']);
+// Source the config.sh file
+const source = spawn('sh', ['-c', 'source /Users/danielhaycraft/Desktop/my_program/config.sh && env']);
+let env = '';
+source.stdout.on('data', (data) => {
+  env += data;
+});
 
-process.on('exit', (code, signal) => {
+source.on('close', (code) => {
+  // Spawn the child process with the updated environment variables
+  const process = spawn('python3', ['/Users/danielhaycraft/Desktop/my_program/alert.py'], {
+    env: Object.assign({}, process.env, env),
+  });
+
+  process.on('exit', (code, signal) => {
     console.log(`Script exited with code ${code} and signal ${signal}`);
+  });
 });
